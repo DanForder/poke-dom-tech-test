e76a757de69da42e9fd671e01ccacfd0b68cf792
Dan Forder <danfrdr@gmail.com>
2022-02-15 16:09:54 +0000

    solution

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+//imports
 import pokemonArray from "./data.js";
 
-console.log(pokemonArray);
+// variables
+const cardContainer = document.querySelector(".card-container");
+
+//functions
+const capitaliseWord = (word) => {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+};
+
+const getTypesHtml = (types) => {
+  if (types.length === 1) {
+    return types[0];
+  }
+  if (types.length === 2) {
+    return `${types[0]} & ${types[1]}`;
+  }
+};
+
+const getPokemonCards = () => {
+  let cardsHtml = "";
+
+  for (const pokemon of pokemonArray) {
+    const pokemonHtml = `<div class="card">
+	  <img
+		class="card__image"
+		src="${pokemon.sprite}"
+		alt="${capitaliseWord(pokemon.name)}"
+	  />
+	  <div class="card__content">
+		<h2 class="card__heading">${capitaliseWord(pokemon.name)}</h2>
+		<p class="card__text">${capitaliseWord(pokemon.name)} (#${
+      pokemon.id
+    }) is a ${getTypesHtml(pokemon.types)} type pokemon.</p>
+	  </div>
+	</div>`;
+
+    cardsHtml += pokemonHtml;
+  }
+  return cardsHtml;
+};
+
+// logic
+cardContainer.innerHTML = getPokemonCards();
